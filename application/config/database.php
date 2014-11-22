de721ef2096f468c2b376ff29846aa3636c5f25b
Mithun Das <mithun.programmer@gmail.com>
2014-11-22 13:52:40 +0530

    db config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'realestate';
+$db['default']['password'] = '865b8674';
 $db['default']['database'] = 'realestate';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
